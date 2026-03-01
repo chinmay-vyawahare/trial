c8d2f27ac7a4f8f01307a16be6b7731a056f60d5
chinmay-vyawahare <chinmay.vyawahare@questt.com>
2026-03-01 12:52:56 +0530

    Delete backend directory

--- backend/app/services/gantt/__init__.py
+++
@@ -1,4 +0,0 @@
-from .service import get_all_sites_gantt, get_dashboard_summary
-from .queries import get_filter_options
-
-__all__ = ["get_all_sites_gantt", "get_dashboard_summary", "get_filter_options"]
